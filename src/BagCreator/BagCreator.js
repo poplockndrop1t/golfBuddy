34f4067c093f77c02f205675a4436763b059ab7a
woonandez <matthew.allen.fernandez@gmail.com>
2020-04-19 09:51:53 -0700

    Add Club Options component

--- src/BagCreator/BagCreator.js
+++ src/BagCreator/BagCreator.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ClubOptions from './ClubOptions/ClubOptions.js';
 import './BagCreator.css';
 
 function BagCreator() {
   return (
     <div className="bagCreatorContainer">
-      <h2>BagCreator</h2>
+      <h2>Bag Creator</h2>
+      <h6>Add your clubs and create your own pdf</h6>
+      <ClubOptions />
     </div>
   )
 };
---
+++ src/BagCreator/ClubOptions/ClubOptions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ClubOptions.css';
+
+function ClubOptions() {
+  return (
+    <div className="bagCreatorContainer">
+      <h2>Club Options</h2>
+    </div>
+  )
+};
+
+export default ClubOptions;
