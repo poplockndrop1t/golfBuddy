520a2f551aa4afc2577a4121b6078998b024f9ff
woonandez <matthew.allen.fernandez@gmail.com>
2020-04-19 09:58:58 -0700

    Pass bag down to bag creator

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      driver: '',
+      driver: 'm4',
       threeWood: '',
       gapWood: '',
       irons: '',
--- src/BagCreator/BagCreator.js
+++ src/BagCreator/BagCreator.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ClubOptions from './ClubOptions/ClubOptions.js';
 import './BagCreator.css';
 
-function BagCreator() {
+function BagCreator({ bag }) {
   return (
     <div className="bagCreatorContainer">
       <h2>Bag Creator</h2>
       <h6>Add your clubs and create your own pdf</h6>
+      <h6>{ bag.driver }</h6>
       <ClubOptions />
     </div>
   )
