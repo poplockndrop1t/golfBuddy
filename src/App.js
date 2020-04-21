90d422a4c8ec5fe95f44a0e8dc5d6c6b2c0a7ed7
woonandez <matthew.allen.fernandez@gmail.com>
2020-04-21 08:44:51 -0700

    Set bag state with nested obj

--- src/App.js
+++ src/App.js
@@ -8,28 +8,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      driver: '',
-      woods: [
-        {
-          'type': '3wood',
-          'brand': 'Titleist'
-        }
-      ],
-      hybrids: [],
-      irons: '',
-      gapWedge: '',
-      sandWedge: '',
-      lobWedge: '',
-      putter: ''
+      bag: {
+        driver: '',
+        woods: [
+          {
+            'type': '3wood',
+            'brand': 'Titleist'
+          }
+        ],
+        hybrids: [],
+        irons: '',
+        gapWedge: '',
+        sandWedge: '',
+        lobWedge: '',
+        putter: ''
+      },
+      newClub: {
+        type: '',
+        brand: ''
+      }
     };
 
     this.setBagState = this.setBagState.bind(this);
   };
 
   setBagState(stateName, newStateValue) {
-    this.setState({
-      [stateName]: newStateValue
-    });
+    const { bag } = { ...this.state };
+    const currentBagState = bag;
+    currentBagState[stateName] = newStateValue;
+    this.setState({ bag: currentBagState });
   }
 
   render() {
@@ -37,7 +44,8 @@ class App extends React.Component {
       <div className="App">
         <Nav />
         <BagCreator
-          bag={this.state}
+          bag={this.state.bag}
+          newClub={this.state.newClub}
           setBagState={this.setBagState} />
         <Footer />
       </div>
--- src/BagCreator/ClubOptions/ClubOptions.js
+++ src/BagCreator/ClubOptions/ClubOptions.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './ClubOptions.css';
 
-function ClubOptions({ setBagState }) {
+var newWood = { type: '', brand: '' };
 
+function addNewWood() {
+
+};
+
+function ClubOptions({ setBagState }) {
   function handleCange(event) {
     return setBagState(event.target.name, event.target.value);
-  }
+  };
+
+  function handleWoodsChange(event) {
+    console.log(event.target.name, event.target.value);
+  };
 
   return (
     <div className="bagCreatorContainer">
@@ -19,8 +28,20 @@ function ClubOptions({ setBagState }) {
           </select>
         </label>
       </div>
-      <div className="woodsContainer">
-
+      <div className="woodsContainer" onChange={ handleWoodsChange }>
+        <select name="woods" >
+          <option value="3+">3+</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <select name="woods" >
+          <option value="TaylorMade">TaylorMade</option>
+          <option value="Titleist">Titleist</option>
+          <option value="Callaway">Callaway</option>
+          <option value="Ping">Ping</option>
+        </select>
+        <button onClick={ addNewWood }>Add Wood</button>
       </div>
     </div>
   )
