3691393071971417d1a9b159fbb10a9b8808f6b3
woonandez <matthew.allen.fernandez@gmail.com>
2020-04-28 09:38:58 -0700

    Bagcreator now renders your bag comp

--- src/BagCreator/BagCreator.js
+++ src/BagCreator/BagCreator.js
@@ -3,18 +3,6 @@ import ClubOptions from './ClubOptions/ClubOptions.js';
 import YourBag from './YourBag/YourBag.js';
 import './BagCreator.css';
 
-function formatClubs(array) {
-  return (
-    <ul>
-      {
-        array.map((wood, key) =>
-           <li key={key}>{wood.type} {wood.brand}</li>
-        )
-      }
-    </ul>
-  )
-};
-
 function BagCreator({ bag, setBagState, createNewClub, newClub, setNewClubValue }) {
   return (
     <div className="bagCreatorContainer">
@@ -27,16 +15,9 @@ function BagCreator({ bag, setBagState, createNewClub, newClub, setNewClubValue 
         setNewClubValue={setNewClubValue}
         createNewClub={createNewClub}
         />
-      <h6>Your Bag</h6>
-      <h6>Driver --- { bag.driver }</h6>
-      <h6>Woods --- { formatClubs(bag.woods) }</h6>
-      <h6>Hybrids --- { formatClubs(bag.hybrids) }</h6>
-      <h6>Irons --- { formatClubs(bag.irons) }</h6>
-      <h6>Wedges --- { formatClubs(bag.wedges) }</h6>
-      <h6>Putter --- { bag.putter }</h6>
+      <YourBag bag={bag} />
     </div>
   )
 };
 
 export default BagCreator;
-
