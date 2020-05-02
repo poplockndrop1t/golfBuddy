c472aa62e878db588f22bdb2b8d0727f5d27f256
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-02 08:46:39 -0700

    Club options comp is no longer stateful

--- src/BagCreator/ClubOptions/ClubOptions.js
+++ src/BagCreator/ClubOptions/ClubOptions.js
@@ -3,75 +3,14 @@ import './ClubOptions.css';
 import ClubSelect from './ClubSelect/ClubSelect.js';
 import AddClubSelect from './AddClubSelect/AddClubSelect.js';
 
-// class ClubOptions extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     handleChange = handleChange.bind(;
-//     handleClubChange = handleClubChange.bind(;
-//   };
-
-//   handleChange(event) {
-//     return props.setBagState(event.target.name, event.target.value);
-//   }; 
-
-//   handleClubChange(event) {
-//     return props.setNewClubValue(event.target.name, event.target.value, event.target.classList[0]);
-//   };
-
-//   render() {
-//     return (
-//       <div className="clubOptionsContainer">
-//         <div className="driverContainer" onChange={handleChange}>
-//           <ClubSelect clubType={"driver"} options={props.brands} />
-//         </div>
-//         <div className="woodsContainer" onChange={handleClubChange}>
-//           <AddClubSelect
-//             createNewClub={ props.createNewClub }
-//             newClub={ props.newClub }
-//             brands={ props.brands}
-//             clubNumbers={ props.clubNumbers }
-//             clubType={"woods"}/>
-//         </div>
-//         <div className="hybridsContainer" onChange={handleClubChange}>
-//           <AddClubSelect
-//             createNewClub={props.createNewClub}
-//             newClub={props.newClub}
-//             brands={props.brands}
-//             clubNumbers={props.clubNumbers}
-//             clubType={"hybrids"} />
-//         </div>
-//         <div className="ironsContainer" onChange={handleClubChange}>
-//           <AddClubSelect
-//             createNewClub={props.createNewClub}
-//             newClub={props.newClub}
-//             brands={props.brands}
-//             clubNumbers={props.ironNumbers}
-//             clubType={"irons"} />
-//         </div>
-//         <div className="wedgesContainer" onChange={handleClubChange}>
-//           <AddClubSelect
-//             createNewClub={props.createNewClub}
-//             newClub={props.newClub}
-//             brands={props.brands}
-//             clubNumbers={props.wedgeNumbers}
-//             clubType={"wedges" } />
-//         </div>
-//         <div className="putterContainer" onChange={handleChange}>
-//           <ClubSelect clubType={"putter"} options={props.brands} />
-//         </div>
-//       </div>
-//     )
-//   }
-// };
-
 function ClubOptions(props) {
+
   function handleChange(event) {
     return props.setBagState(event.target.name, event.target.value);
   };
 
   function handleClubChange(event) {
-    //     return props.setBagState(event.target.name, event.target.value);
+    return props.setNewClubValue(event.target.name, event.target.value, event.target.classList[0]);
   };
 
   return (
@@ -116,6 +55,6 @@ function ClubOptions(props) {
         </div>
       </div>
   )
-}
+};
 
 export default ClubOptions;
