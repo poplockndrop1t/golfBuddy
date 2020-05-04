2539b9b904e20116b5ec230a18e42c335c5a29ee
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-04 11:11:05 -0700

    Inject store into provider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
 import store from './redux/store/store';
-import { Provider } from "react-redux";
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App store={store} />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
