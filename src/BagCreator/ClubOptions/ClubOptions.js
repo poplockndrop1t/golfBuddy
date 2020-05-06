30f8085bcd50119212e2eb850519d5096783e61b
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-06 08:43:37 -0700

    Club options are now rendered from a map

--- src/BagCreator/ClubOptions/ClubOptions.js
+++ src/BagCreator/ClubOptions/ClubOptions.js
@@ -6,71 +6,31 @@ import './ClubOptions.css';
 
 function ClubOptions(props) {
 
-  function handleChange(event) {
-    return props.setDriverOrPutter(event.target.name, event.target.value, 1);
-  };
+  var clubTypeArray = Object.keys(props.bag);
+  var clubValuesArray = Object.values(props.bag);
 
   function handleClubChange(event) {
-    console.log('handle club change', event.target.name, event.target.value, event.target.classList[0])
     return props.setNewClubValue(event.target.name, event.target.value, event.target.classList[0]);
   };
 
   return (
     <div className="clubOptionsContainer">
-      <div className="driverContainer" onChange={handleClubChange}>
-        <AddClubSelect
-          createNewClub={ props.createNewClub }
-          newClub={ props.newClub }
-          brands={ props.brands }
-          clubNumbers={ props.clubNumbers }
-          clubType={"driver"}
-        />
-      </div>
-      <div className="woodsContainer" onChange={handleClubChange}>
-        <AddClubSelect
-          createNewClub={ props.createNewClub }
-          newClub={ props.newClub }
-          brands={ props.brands}
-          clubNumbers={ props.clubNumbers }
-          clubType={"woods"}
-        />
-      </div>
-      <div className="hybridsContainer" onChange={handleClubChange}>
-        <AddClubSelect
-          createNewClub={props.createNewClub}
-          newClub={props.newClub}
-          brands={props.brands}
-          clubNumbers={props.clubNumbers}
-          clubType={"hybrids"}
-        />
-      </div>
-      <div className="ironsContainer" onChange={handleClubChange}>
-        <AddClubSelect
-          createNewClub={props.createNewClub}
-          newClub={props.newClub}
-          brands={props.brands}
-          clubNumbers={props.ironNumbers}
-          clubType={"irons"}
-        />
-      </div>
-      <div className="wedgesContainer" onChange={handleClubChange}>
-        <AddClubSelect
-          createNewClub={props.createNewClub}
-          newClub={props.newClub}
-          brands={props.brands}
-          clubNumbers={props.wedgeNumbers}
-          clubType={"wedges" }
-        />
-      </div>
-      <div className="putterContainer" onChange={handleClubChange}>
-        <AddClubSelect
-          createNewClub={props.createNewClub}
-          newClub={props.newClub}
-          brands={props.brands}
-          clubNumbers={props.wedgeNumbers}
-          clubType={"putter"}
-        />
-      </div>
+      {
+        clubTypeArray.map((clubType, key) => {
+          return (
+            <div onChange={handleClubChange}>
+              <AddClubSelect
+                key={key}
+                createNewClub={props.createNewClub}
+                newClub={props.newClub}
+                brands={props.brands}
+                clubNumbers={props.clubNumbers}
+                clubType={clubType}
+              />
+            </div>
+          )
+        })
+      }
     </div>
   )
 };
