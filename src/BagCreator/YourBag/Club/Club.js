5feff79a40eafd016c3ab555e2e0bf0bcdc05f9e
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-06 06:49:35 -0700

    Add helper methods

--- src/BagCreator/YourBag/Club/Club.js
+++ src/BagCreator/YourBag/Club/Club.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import './Club.css';
 
-function handleClick(club, clubType, id) {
-  console.log('here', club, clubType, id)
-};
+// function handleClick(club, clubType, id) {
+//   console.log('here', club, clubType, id)
+// };
 
-function formatClub(club, clubType, id, props) {
-  return (
-    <div className="club">
-      <h3>{clubType}</h3>
-      <p>{club}</p>
-      <button onClick={ () => handleClick(club, clubType, id) }>Remove Club</button>
-    </div>
-  )
-};
+// function formatClub(club, clubType, id, props) {
+//   return (
+//     <div className="club">
+//       <h3>{clubType}</h3>
+//       <p>{club}</p>
+//       <button onClick={ () => handleClick(club, clubType, id) }>Remove Club</button>
+//     </div>
+//   )
+// };
 
-function formatClubsArray(props) {
-  return (
-    <div className="club">
-      <h3>{props.clubType}</h3>
-      {
-        props.club.map((club, key) => {
-          return (
-            <p key={key}>{club.type} {club.brand}</p>
-          )
-        })
-      }
-    </div>
-  )
-};
+// function formatClubsArray(props) {
+//   return (
+//     <div className="club">
+//       <h3>{props.clubType}</h3>
+//       {
+//         props.club.map((club, key) => {
+//           return (
+//             <p key={key}>{club.type} {club.brand}</p>
+//           )
+//         })
+//       }
+//     </div>
+//   )
+// };
 
 // function Club(props) {
 //   if (typeof props.club === 'string') {
@@ -45,7 +45,40 @@ function formatClubsArray(props) {
 class Club extends React.Component {
   constructor(props) {
     super(props);
+
+    handleClick = handleClick.bind(this);
+    formatClub = formatClub.bind(this);
+    formatClubsArray = formatClubsArray.bind(this);
   }
+
+  handleClick(club, clubType, id) {
+    console.log('here', club, clubType, id)
+  };
+
+  formatClub(club, clubType, id, props) {
+    return (
+      <div className="club">
+        <h3>{clubType}</h3>
+        <p>{club}</p>
+        <button onClick={ () => handleClick(club, clubType, id) }>Remove Club</button>
+      </div>
+    )
+  };
+
+  formatClubsArray(props) {
+    return (
+      <div className="club">
+        <h3>{props.clubType}</h3>
+        {
+          props.club.map((club, key) => {
+            return (
+              <p key={key}>{club.type} {club.brand}</p>
+            )
+          })
+        }
+      </div>
+    )
+  };
 
   render() {
     if (typeof this.props.club === 'string') {
