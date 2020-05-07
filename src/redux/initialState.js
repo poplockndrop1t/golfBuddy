6d839d8c99c05ea89bcbf444510ddbb3efc00e64
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-07 07:25:08 -0700

    Initial state is now its own file

---
+++ src/redux/initialState.js
@@ -0,0 +1,18 @@
+const initialState = {
+  bag: {
+    driver: [],
+    woods: [],
+    hybrids: [],
+    irons: [],
+    wedges: [],
+    putter: []
+  },
+  bagSize: 0,
+  newClub: {
+    category: '',
+    type: '',
+    brand: ''
+  }
+};
+
+export default initialState;
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,28 +1,11 @@
 import clubReducer from './clubReducer.js';
+import initialState from '../initialState.js';
 import {
   ADD_NEW_CLUB,
   DECREMENT_BAG_SIZE,
   INCREMENT_BAG_SIZE,
   REMOVE_CLUB,
   SET_NEW_CLUB_VALUE } from '../constants/constants';
-
-
-const initialState = {
-  bag: {
-    driver: [],
-    woods: [],
-    hybrids: [],
-    irons: [],
-    wedges: [],
-    putter: []
-  },
-  bagSize: 0,
-  newClub: {
-    category: '',
-    type: '',
-    brand: ''
-  }
-};
 
 function rootReducer(state = initialState, action) {
   const newState = {...state};
