990c588f394442b69a826874e2ee742615dd4981
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-07 10:46:36 -0700

    Code cleanup

--- src/redux/reducers/clubReducer.js
+++ src/redux/reducers/clubReducer.js
@@ -10,12 +10,11 @@ function clubReducer(state = [], action) {
   switch(action.type) {
     case `${ADD_NEW_CLUB}`:
       const cleanClub = { category: '', type: '', brand: '' };
-      newState['bag'][newState.newClub.category].push(newState.newClub)
+      newState['bag'][newState.newClub.category].push(newState.newClub);
       newState.newClub = cleanClub;
       return newState;
     case `${REMOVE_CLUB}`:
       newState['bag'][action.payload.type].splice(action.payload.i, 1);
-      // console.log('newState', action.payload);
       return newState;
     case `${SET_NEW_CLUB_VALUE}`:
       newState.newClub = action.payload;
