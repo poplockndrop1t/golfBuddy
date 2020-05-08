cace7519f30c0a5eb7b4deba053307d296fea4d0
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-08 06:29:42 -0700

    Code cleanup

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,13 +1,20 @@
 import { combineReducers } from 'redux';
 import clubReducer from './clubReducer.js';
 import bagCountReducer from './bagCountReducer';
-// import initialState from '../initialState.js';
-import {
-  ADD_NEW_CLUB,
-  REMOVE_CLUB,
-  SET_NEW_CLUB_VALUE } from '../constants/constants';
 
-function rootReducer(state = [], action) {
+const initialState = {
+  brands: ["", "TaylorMade", "Titleist", "Callaway", "Ping"],
+  clubTypeNumbers: {
+    driver: ["", "8.5", "9.5", "10.0", "10.5"],
+    hybrids: ["", "2", "3", "4", "5", "6"],
+    irons: ["", "3p", "4a", "4p", "5a", "5p"],
+    putter: ["", "33", "34", "35", "36", "37", "38"],
+    wedges: ["", "46", "48", "50", "52", "54", "56", "58", "60"],
+    woods: ["", "13.5", "15", "16.5", "19", "21"]
+  }
+}
+
+function rootReducer(state = initialState, action) {
   switch(action.type) {
     default:
       return state
