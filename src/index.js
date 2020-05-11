d2f89fa2195618301a6ecf72085e3cfbe3a7a4ef
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-11 08:24:24 -0700

    Add base router code

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Route path="/" component={App} />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
