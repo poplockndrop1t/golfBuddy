8dc2eebc26ecb85b23b6d37852834baf7b820191
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-11 08:22:28 -0700

    Add react router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './redux/store/store';
 import './index.css';
 
