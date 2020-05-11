bfbd6f338ec2aa4e7a27fb85462b67393182ac02
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-11 08:27:18 -0700

    Add filter to app comp

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <Route path="/" component={App} />
+        <Route path="/:filter?" component={App} />
       </Router>
     </Provider>
   </React.StrictMode>,
