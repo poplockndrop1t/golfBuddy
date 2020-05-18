94882e584594061fa350a7b5fccad00ceb5e6d6a
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-18 08:05:03 -0700

    Express server now serves up react app

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const port = process.env.PORT || 1337;
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/build/index.html');
+});
 
 app.listen(port, () => {
   console.log(`|2 U ${port}`);
