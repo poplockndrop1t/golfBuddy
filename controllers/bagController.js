8164eb0cf0380d1d57895310003e43762ab73e9a
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-29 09:58:52 -0700

    Implement base find function

--- controllers/bagController.js
+++ controllers/bagController.js
@@ -1,5 +1,13 @@
+const Bag = require('../db/models/bag.js');
+
 module.exports = {
   get: function(req, res) {
-    console.log(req);
+    Bag.find({}, (err, response) => {
+      if (response) {
+        res.json(response);
+      } else {
+        res.json(err);
+      }
+    });
   }
 }
