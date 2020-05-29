dbd15831f493b1b50cf192d74282da84a9f5f7c2
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-29 16:21:49 -0700

    Handle for club updates with post

--- controllers/bagController.js
+++ controllers/bagController.js
@@ -11,7 +11,7 @@ module.exports = {
     });
   },
   post: function(req, res) {
-    Bag.find({}, (err, response) => {
+    Bag.findOne({}, (err, response) => {
       if (response) {
         if (response.length === 0) {
           var newBag = new Bag(req.body);
@@ -19,6 +19,18 @@ module.exports = {
             if (err) return console.error(err);
             res.json(response);
           })
+        } else {
+          var currentBag = response;
+          currentBag.driver = req.body.driver;
+          currentBag.woods = req.body.woods;
+          currentBag.hybrids = req.body.hybrids;
+          currentBag.irons = req.body.irons;
+          currentBag.wedges = req.body.wedges;
+          currentBag.putter = req.body.putter;
+          currentBag.save((err, data) => {
+            if (err) return console.error(err);
+            res.json(response);
+          });
         }
       } else {
         res.json(err);
