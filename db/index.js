b16fbe0d7e586291d2d45352918e2b41c11d1e05
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-29 09:16:42 -0700

    Uri and options now passed as variables

--- db/index.js
+++ db/index.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const mongodb_uri = 'mongodb://localhost:27017/golfbuddy';
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
-
-mongoose.connect(mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(mongodb_uri, options);
 
 const db = mongoose.connection;
 
-db.once('open', () => {
-  console.log('connected to db');
-});
+db.once('open', () => console.log('connected to db'));
 
 module.exports = db;
