2b975facf0acdabcd27aa0948506c2f44fa345fe
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-29 09:31:22 -0700

    Add base controller and base router

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db/index.js');
+const router = require('./router.js');
 
 const app = express();
 const port = process.env.PORT || 1337;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/build'));
+
+app.use('/bag', router);
 
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/build/index.html');
