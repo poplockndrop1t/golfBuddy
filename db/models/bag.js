2401c62aabc2afc124753438d806ee75db405c76
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-29 09:21:06 -0700

    Add additional data modeling for bag

--- db/models/bag.js
+++ db/models/bag.js
@@ -4,12 +4,42 @@ const Scheme = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
 const bagSchema = mongoose.Schema({
-  driver: Array,
-  woods: Array,
-  hybrids: Array,
-  irons: Array,
-  wedges: Array,
-  putter: Array
+  driver: [{
+    category: String,
+    type: String,
+    brand: String,
+    flex: String
+  }],
+  woods: [{
+    category: String,
+    type: String,
+    brand: String,
+    flex: String
+  }],
+  hybrids: [{
+    category: String,
+    type: String,
+    brand: String,
+    flex: String
+  }],
+  irons: [{
+    category: String,
+    type: String,
+    brand: String,
+    flex: String
+  }],
+  wedges: [{
+    category: String,
+    type: String,
+    brand: String,
+    flex: String
+  }],
+  putter: [{
+    category: String,
+    type: String,
+    brand: String,
+    flex: String
+  }]
 });
 
 const Bag = mongoose.model('Bag', bagSchema);
