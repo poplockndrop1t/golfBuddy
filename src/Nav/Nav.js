7ca21ea094fd7f5facb925331ebb3f0fac90a95d
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-31 11:20:28 -0700

    Add emoji button to the nav bar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
-
 function Nav() {
   return (
     <div className="navContainer">
+      <div>
+        <button>
+          <span role="img" aria-label="golf buddy">⛳️</span>
+        </button>
+      </div>
       <Link to="/">
         <p>Landing</p>
       </Link>
