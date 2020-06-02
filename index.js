02b9a3f1061ca065234c2db5d4a593ce48ae8bca
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-02 07:17:55 -0700

    Add base session code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const db = require('./db/index.js');
 const router = require('./router.js');
 
@@ -10,11 +11,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/build'));
 
+app.use(session({
+  secret: 'golfbuddy',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60000,
+    secure: false
+  }
+}));
+
+app.set('trust proxy', 1)
+
 app.use('/api', router);
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/build/index.html');
+app.get('/', function(req, res, next) {
+    console.log(req.session);
 });
+
+// app.get('*', (req, res) => {
+//   res.sendFile(__dirname + '/build/index.html');
+// });
 
 app.listen(port, () => {
   console.log(`|2 U ${port}`);
