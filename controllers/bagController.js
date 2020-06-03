1b259736890a9ae69a6f292f1f500e7d7e2aabf0
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-03 09:54:02 -0700

    Users can now update their bag based on username

--- controllers/bagController.js
+++ controllers/bagController.js
@@ -2,7 +2,7 @@ const Bag = require('../db/models/bag.js');
 
 module.exports = {
   delete: function(req, res) {
-    Bag.findOne({ _id: '5ed7d1a5e7445509119e2d7d' }, (err, response) => {
+    Bag.findOne({ _id: '5ed7d10177a8c708d6279137' }, (err, response) => {
       Bag.deleteOne(response, (e, r) => {
         if (e) console.log(e)
         res.send(r);
@@ -12,38 +12,33 @@ module.exports = {
   get: function(req, res) {
     Bag.find({}, (err, response) => {
       if (err) return console.error(err);
-      console.log('here', response);
       res.send(JSON.stringify(response));
     });
   },
   post: function(req, res) {
-    Bag.findOne({ username: req.body.username }, (err, response) => {
-      if (err) return console.error(err);
-      if (response === null) {
+    Bag.findOne({ username: req.body.username }, (error, response) => {
+      if (error) return console.error(error);
+      if (response === null && req.body.username) {
         var newBag = new Bag()
         newBag.username = req.body.username;
-        newBag.save((e, data) => {
-          if (e) return console.error(e);
+        newBag.save((err, data) => {
+          if (err) return console.error(err);
           res.json(data);
         });
       }
       if (response) {
-        console.log('here', response);
+        var currentBag = response;
+        currentBag.driver = req.body.driver ? req.body.driver : currentBag.driver;
+        currentBag.woods = req.body.woods ? req.body.woods : currentBag.woods;
+        currentBag.hybrids = req.body.hybrids ? req.body.hybrids : currentBag.hybrids;
+        currentBag.irons = req.body.irons ? req.body.irons : currentBag.irons;
+        currentBag.wedges = req.body.wedges ? req.body.wedges : currentBag.wedges;
+        currentBag.putter = req.body.putter ? req.body.putter : currentBag.putter;
+        currentBag.save((err, data) => {
+          if (err) return console.error(err);
+          res.json(currentBag);
+        });
       }
-      // else {
-        // console.log('test,', response);
-        // var currentBag = response;
-        // currentBag.driver = req.body.driver;
-        // currentBag.woods = req.body.woods;
-        // currentBag.hybrids = req.body.hybrids;
-        // currentBag.irons = req.body.irons;
-        // currentBag.wedges = req.body.wedges;
-        // currentBag.putter = req.body.putter;
-        // currentBag.save((err, data) => {
-        //   if (err) return console.error(err);
-        //   res.json(response);
-        // });
-      // };
     });
   },
   retrieveBag: function(req, res, cb) {
