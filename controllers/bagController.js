1b97c9ddfaacd0fc2c2fc5025c8d760623a88ba6
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-03 09:34:40 -0700

    Back end can now pull up a bag based on username

--- controllers/bagController.js
+++ controllers/bagController.js
@@ -17,29 +17,39 @@ module.exports = {
     });
   },
   post: function(req, res) {
-    Bag.findOne({}, (err, response) => {
+    Bag.findOne({ username: req.body.username }, (err, response) => {
       if (err) return console.error(err);
-      if (response) {
-        if (response.length === 0) {
-          var newBag = new Bag(req.body);
-          newBag.save((err, data) => {
-            if (err) return console.error(err);
-            res.json(response);
-          });
-        } else {
-          var currentBag = response;
-          currentBag.driver = req.body.driver;
-          currentBag.woods = req.body.woods;
-          currentBag.hybrids = req.body.hybrids;
-          currentBag.irons = req.body.irons;
-          currentBag.wedges = req.body.wedges;
-          currentBag.putter = req.body.putter;
-          currentBag.save((err, data) => {
-            if (err) return console.error(err);
-            res.json(response);
-          });
-        }
-      };
+      if (response === null) {
+        var newBag = new Bag()
+        newBag.username = req.body.username;
+        console.log(newBag, req.body);
+        newBag.save((e, data) => {
+          if (e) return console.error(e);
+          res.json(data);
+        });
+      }
+
+      // if (response) {
+      //   if (response.length === 0) {
+      //     var newBag = new Bag(req.body);
+      //     newBag.save((err, data) => {
+      //       if (err) return console.error(err);
+      //       res.json(response);
+      //     });
+      //   } else {
+      //     var currentBag = response;
+      //     currentBag.driver = req.body.driver;
+      //     currentBag.woods = req.body.woods;
+      //     currentBag.hybrids = req.body.hybrids;
+      //     currentBag.irons = req.body.irons;
+      //     currentBag.wedges = req.body.wedges;
+      //     currentBag.putter = req.body.putter;
+      //     currentBag.save((err, data) => {
+      //       if (err) return console.error(err);
+      //       res.json(response);
+      //     });
+      //   }
+      // };
     });
   },
   retrieveBag: function(req, res, cb) {
