010db211939555c49aeac620921407bedf6493ab
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-08 07:33:46 -0700

    Import bcrypt and resolve bcrypt issue with rebuild

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,11 @@
+const bcrypt = require('bcrypt');
 const Bag = require('../db/models/bag.js');
+const saltRounds = require('../secret/secret.js');
 
 module.exports = {
+  createUser: function(req, res) {
+
+  },
   login: function(req, res) {
 
   }
