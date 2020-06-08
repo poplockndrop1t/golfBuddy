650470abd581195843e1f61daf06d2af055e81b3
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-08 07:49:43 -0700

    Add bcrypt has and salt code and check for user

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,7 +6,19 @@ module.exports = {
   createUser: function(req, res) {
     Bag.find({ username: req.body.username }, (err, response) => {
       if (err) return console.error(err);
-      res.send(JSON.stringify(`USER EXISTS`));
+      if (response.length > 0) return res.send(JSON.stringify(`USER EXISTS`));
+      bcrypt.genSalt(saltRounds.saltRounds, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+          var newBag = new Bag();
+          newBag.username = req.body.username;
+          newBag.password = hash;
+          console.log(newBag);
+          // newBag.save((err, data) => {
+          //   if (err) return console.error(err);
+          //   res.json(data);
+          // });
+        });
+      });
     });
   },
   login: function(req, res) {
