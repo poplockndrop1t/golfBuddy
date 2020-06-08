b53db6d346072f0a2da3b2eec49bdf4db96eb9b7
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-08 07:37:11 -0700

    Add create user route and fill out base createuser method

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,10 @@ const saltRounds = require('../secret/secret.js');
 
 module.exports = {
   createUser: function(req, res) {
-
+    Bag.find({ username: req.body.username }, (err, response) => {
+      if (err) return console.error(err);
+      res.send(JSON.stringify(`USER EXISTS`));
+    });
   },
   login: function(req, res) {
 
