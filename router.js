81d8dba8d172562da2e1fa8b3bb7bf5f67f69006
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-08 07:01:21 -0700

    Login method now gets called on api

--- router.js
+++ router.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const bagController = require('./controllers/bagController.js');
+const loginController = require('./controllers/loginController.js');
 
+// Bag Routes
 router.get('/bag', bagController.get);
 router.post('/bag', bagController.post);
 router.delete('/bag', bagController.delete);
+
+// Login Routes
+router.post('/login', loginController.login);
 
 module.exports = router;
