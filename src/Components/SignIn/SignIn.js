1f2c8f966be815f4a319c0af14b7df2f939a0fcc
woonandez <matthew.allen.fernandez@gmail.com>
2020-06-08 09:31:14 -0700

    Add verify string function

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -2,12 +2,29 @@ import React from 'react';
 import './SignIn.css';
 
 function SignIn() {
+  var username;
+  var password;
+
+  function verifyString(string) {
+    var result = true;
+    for (var i = 0; i < string.length; i++) {
+      if (/[a-z]/gi.test(string[i]) === false) {
+        result = false;
+      }
+    }
+    return result;
+  };
+
+  function handleChange(event) {
+    console.log('event', event.target.name, event.target.value, test);
+  };
+
   return (
     <div className="signInContainer">
       <div className="inputContainer">
         <h2>Sign In</h2>
-        <input placeholder="username" name="username" />
-        <input placeholder="password" type="password" password="password" />
+        <input placeholder="username" name="username" onChange={handleChange} />
+        <input placeholder="password" type="password" name="password" onChange={handleChange} />
         <input type="submit" value="submit" />
       </div>
     </div>
