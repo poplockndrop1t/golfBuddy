c924514a0eb2451b420d77e8681e0b62448e52b1
poplockndrop1t <matthew.allen.fernandez@gmail.com>
2020-06-25 13:55:30 -0700

    The stats comp has a section for new stats and stats display

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -4,33 +4,37 @@ import './Stats.css';
 function Stats(props) {
   return (
     <div className="statsContainer">
-      <h2>Stats</h2>
       <form>
-        <label for="fir">FIR</label>
+        <h2>Add Your Round</h2>
+        <label>FIR</label>
         <input id="fir"></input>
-        <label for="firAttempts">FIR Attempts</label>
+        <label>FIR Attempts</label>
         <input id="firAttempts"></input>
-        <label for="gir">FIR</label>
+        <label>FIR</label>
         <input id="gir"></input>
-        <label for="girAttempts">FIR Attempts</label>
+        <label>FIR Attempts</label>
         <input id="girAttempts"></input>
-        <label for="upAndDown30Yard">Total Up And Downs within 30 Yards</label>
+        <label>Total Up And Downs within 30 Yards</label>
         <input id="upAndDown30Yard"></input>
-        <label for="upAndDown30YardAttempts">Attempts at Up And Downs within 30 Yards</label>
+        <label>Attempts at Up And Downs within 30 Yards</label>
         <input id="upAndDown30YardAttempts"></input>
-        <label for="wedgeAppInside100">Total Wedge Approaches within 6ft inside 100 yards.</label>
+        <label>Total Wedge Approaches within 6ft inside 100 yards.</label>
         <input id="wedgeAppInside100"></input>
-        <label for="wedgeAppInside100Attempts">Wedge Approach Attempts within 6ft inside 100 yards.</label>
+        <label>Wedge Approach Attempts within 6ft inside 100 yards.</label>
         <input id="wedgeAppInside100Attempts"></input>
-        <label for="puttsMadeWithin12Ft">Total Putts made within 12 Ft.</label>
+        <label>Total Putts made within 12 Ft.</label>
         <input id="puttsMadeWithin12Ft"></input>
-        <label for="puttsMadeWithin12FtAttempts">Attempts at Putts Made Within 12 Ft</label>
+        <label>Attempts at Putts Made Within 12 Ft</label>
         <input id="puttsMadeWithin12FtAttempts"></input>
-        <label for="score">Score</label>
+        <label>Score</label>
         <input id="score"></input>
-        <label for="par">Par</label>
+        <label>Par</label>
         <input id="par"></input>
+        <input type="submit" value="Add Stats"></input>
       </form>
+      <div>
+        <h2>Stats Display</h2>
+      </div>
     </div>
   )
 };
