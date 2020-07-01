43b09aaa280ebaba4f1299e81b04ea71c1e3ca71
poplockndrop1t <matthew.allen.fernandez@gmail.com>
2020-07-01 08:57:15 -0700

    Stats reducer default state now matches mongoose dm

--- src/redux/reducers/addStatsReducer.js
+++ src/redux/reducers/addStatsReducer.js
@@ -1,14 +1,20 @@
 import { ADD_STATS } from '../constants/constants';
 
 const initialState = {
-  stats: {
-    driver: [],
-    woods: [],
-    hybrids: [],
-    irons: [],
-    wedges: [],
-    putter: []
-  }
+  stats: [{
+    fir: 0,
+    firAttempts: 0,
+    gir: 0,
+    girAttempts: 0,
+    upAndDown30Yard: 0,
+    upAndDown30YardAttempts: 0,
+    wedgeAppInside100: 0,
+    wedgeAppInside100Atempts: 0,
+    puttsMadeWithin12Ft: 0,
+    puttsMadeWithin12FtAttempts: 0,
+    score: 0,
+    par: 0
+  }]
 };
 
 function addStatsReducer(state = initialState, action) {
